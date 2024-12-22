743273cfdb4b3d8ab1d27940168e4e965134c6ce
Mamello <mmoshoeshoe@webelieve.co.za>
2024-12-22 21:11:41 +0200

    create process placeholder

---
+++ src/app/components/process-section/process-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './process-section.component.html',
+  styleUrl: './process-section.component.scss'
+})
+export class ProcessSectionComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
+import { ProcessSectionComponent } from '../../components/process-section/process-section.component';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +10,8 @@ import { BenefitsComponent } from '../../components/benefits/benefits.component'
   imports: [
     BannerComponent,
     ToolbarComponent,
-    BenefitsComponent
+    BenefitsComponent,
+    ProcessSectionComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
