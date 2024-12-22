11fb71fd67969144f7ef312597ba429135565584
Mamello <mmoshoeshoe@webelieve.co.za>
2024-12-22 19:28:50 +0200

    create banner

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    BannerComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
