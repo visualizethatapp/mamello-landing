e584742bc669caea02082f78d332712e601d268e
Mamello <mmoshoeshoe@webelieve.co.za>
2024-12-22 20:04:04 +0200

    add toolbar to homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    BannerComponent
+    BannerComponent,
+    ToolbarComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
