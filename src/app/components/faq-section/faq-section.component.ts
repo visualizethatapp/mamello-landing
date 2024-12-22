e6830a3de072e57656d47ddc67e16dd85a0e52aa
Mamello <mmoshoeshoe@webelieve.co.za>
2024-12-22 21:19:17 +0200

    create placeholder for faq

---
+++ src/app/components/faq-section/faq-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './faq-section.component.html',
+  styleUrl: './faq-section.component.scss'
+})
+export class FaqSectionComponent {
+
+}
