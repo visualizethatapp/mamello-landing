deafc944799d62c8614986752439fe0687a42ce9
Mamello <mmoshoeshoe@webelieve.co.za>
2025-01-06 17:05:49 +0200

    update banner font family

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
 import { ProcessSectionComponent } from '../../components/process-section/process-section.component';
 import { FaqSectionComponent } from '../../components/faq-section/faq-section.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
@@ -13,7 +14,8 @@ import { FaqSectionComponent } from '../../components/faq-section/faq-section.co
     ToolbarComponent,
     BenefitsComponent,
     ProcessSectionComponent,
-    FaqSectionComponent
+    FaqSectionComponent,
+    FooterComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
